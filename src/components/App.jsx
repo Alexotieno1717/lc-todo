45fbd86f0addab43f02f8d62bd565d5a617130d3
Alexotieno1717 <alexotieno900@gmail.com>
2022-06-17 11:21:11 +0300

    Episode 4 - Event listeners

---
+++ src/components/App.jsx
@@ -0,0 +1,120 @@
+import { useState } from 'react';
+import '../reset.css';
+import '../App.css';
+
+function App() {
+   
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish React Series',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Go Grocery',
+      isComplete: true,
+    },
+    {
+      id: 3,
+      title: 'Take over world',
+      isComplete: false,
+    },
+  ]);
+
+  const [todoInput, setTodosInput] = useState('');
+  const [idFortodo, setIdForTodo] = useState(4);
+
+  function addTodo(event) {
+    event.preventDefault();
+
+    if (todoInput.trim().length === 0) {
+      return;
+    }
+
+    setTodos([...todos, {
+      id: idFortodo,
+      title: todoInput,
+      isComplete: false
+    },
+  ]);
+
+  setTodosInput('');
+  setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
+  }
+
+  function deleteTodo(id) {
+     setTodos([... todos].filter(todo => todo.id !== id));
+  }
+
+  function handleInput(event) {
+    setTodosInput(event.target.value)
+  }
+
+  return (
+    <div className="todo-app-container">
+      <div className="todo-app">
+        <h2>Todo App</h2>
+        <form action="#" onSubmit={ addTodo }>
+          <input
+            type="text"
+            value={todoInput}
+            onChange={handleInput}
+            className="todo-input"
+            placeholder="What do you need to do?"
+          />
+        </form>
+
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <li key={todo.id} className="todo-item-container">
+              <div className="todo-item">
+                <input type="checkbox" />
+                <span className="todo-item-label">{todo.title}</span>
+                {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
+              </div>
+              <button onClick={() => deleteTodo(todo.id)} className="x-button">
+                <svg
+                  className="x-button-icon"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <div className="check-all-container">
+          <div>
+            <div className="button">Check All</div>
+          </div>
+
+          <span>3 items remaining</span>
+        </div>
+
+        <div className="other-buttons-container">
+          <div>
+            <button className="button filter-button filter-button-active">
+              All
+            </button>
+            <button className="button filter-button">Active</button>
+            <button className="button filter-button">Completed</button>
+          </div>
+          <div>
+            <button className="button">Clear completed</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
