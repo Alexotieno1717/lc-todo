71da8176889df3cb8c37e57064ebfee515fa69f8
Alexotieno1717 <alexotieno900@gmail.com>
2022-06-21 12:00:16 +0300

    Episode 8 - Other Built-in React Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NoTodos from './NoTodos';
 import '../reset.css';
 import '../App.css';
@@ -6,7 +6,9 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
-   
+
+  const [name, setName] = useState('');
+  const nameInputEl = useRef(null);
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -115,11 +117,30 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    // console.log('use effect running');
+    nameInputEl.current.focus();
+  }, []);
+
 
 
   return (
     <div className="todo-app-container">
       <div className="todo-app">
+        <div className="name-container">
+          <h2>What is your Name?</h2>
+           <form action="#">
+            <input
+              type="text"
+              ref={nameInputEl}
+              className="todo-input"
+              placeholder="What is your name"
+              value={name}
+              onChange={event => setName(event.target.value)}
+            />
+           </form>
+          {name && <p className="name-lable">Hello, {name}</p>}
+        </div>
         <h2>Todo App</h2>
 
         <TodoForm addTodo={addTodo} />
